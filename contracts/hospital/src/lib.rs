b99c437f8fa48de84686fe9131f56fcf658ca928
udodinho <udodinho200@gmail.com>
2025-05-13 19:59:58 +0100

    feat: Implement medical records update

--- contracts/hospital/src/lib.rs
+++ contracts/hospital/src/lib.rs
@@ -95,7 +95,7 @@ impl HospitalContract {
    pub fn initialize(env: Env, admin: Address) -> Address {
 
     if env.storage().instance().has(&DataKey::Admin) {
-        panic!("Contract already initilized");
+        panic!("Contract already initialized");
     }
 
     env.storage().instance().set(&DataKey::Admin, &admin);
@@ -316,8 +316,22 @@ impl HospitalContract {
    ) -> u64 {
     Self::check_admin(&env);
 
+    let patient: Patient = env.storage().instance().get(&DataKey::Patient(patient_id)).unwrap_or_else(|| panic!("Patient not found"));
+
+    let doctor: Doctor = env.storage().instance().get(&DataKey::Doctor(doctor_id)).unwrap_or_else(|| panic!("Doctor not found"));
+
+    if !patient.active {
+        panic!("Patient is inactive");
+    }
+
+    if !doctor.active {
+        panic!("Doctor is inactive");
+    }
+
     let medical_test_count: u64 = env.storage().instance().get(&DataKey::TestCount).unwrap_or(0);
     let new_id = medical_test_count + 1;
+
+
 
     let medical_test_records = MedicalTest {
         id: new_id,
@@ -331,6 +345,14 @@ impl HospitalContract {
 
     env.storage().instance().set(&DataKey::MedicalTest(new_id), &medical_test_records);
     env.storage().instance().set(&DataKey::TestCount, &new_id);
+
+    let mut patient_tests: Vec<u64> = env.storage().instance().get(&DataKey::PatientTests(patient_id)).unwrap();
+    patient_tests.push_back(new_id);
+    env.storage().instance().set(&DataKey::PatientTests(patient_id), &patient_tests);
+
+    let mut doctor_tests: Vec<u64> = env.storage().instance().get(&DataKey::DoctorTests(doctor_id)).unwrap();
+    doctor_tests.push_back(new_id);
+    env.storage().instance().set(&DataKey::DoctorTests(doctor_id), &doctor_tests);
     
     new_id
    }
@@ -342,18 +364,62 @@ impl HospitalContract {
     }
     }  
 
-   pub fn get_all_patient_medical_records(env: Env, patient_id: u64) -> MedicalTest {
-    match env.storage().instance().get(&DataKey::MedicalTest(patient_id)) {
-        Some(medical_test) => medical_test,
-        None => panic!("No medical test record found"),
+    pub fn update_medical_test (
+        env: Env,
+        id: u64,
+        test_type: String,
+        test_date: u64,
+        results: String,
+        notes: String,
+       ) -> MedicalTest {
+        Self::check_admin(&env);
+    
+        let mut test: MedicalTest = env.storage().instance().get(&DataKey::MedicalTest(id))
+            .unwrap_or_else(|| panic!("Medical test not found"));
+    
+        test.test_type = test_type;
+        test.test_date = test_date;
+        test.results = results;
+        test.notes = notes;
+    
+        env.storage().instance().set(&DataKey::MedicalTest(id), &test);
+    
+        test
     }
-    }  
 
-   pub fn get_all_doctor_medical_records(env: Env, doctor_id: u64) -> MedicalTest {
-    match env.storage().instance().get(&DataKey::MedicalTest(doctor_id)) {
-        Some(medical_test) => medical_test,
-        None => panic!("No medical test record found"),
+    pub fn get_patients_tests(env: Env, patient_id: u64) -> Vec<MedicalTest> {
+
+        if !env.storage().instance().has(&DataKey::Patient(patient_id)) {
+            panic!("Patient not found");
+        }
+
+        let test_ids: Vec<u64> = env.storage().instance().get(&DataKey::PatientTests(patient_id)).unwrap();
+        let mut tests = Vec::new(&env);
+
+        for test_id in test_ids.iter() {
+            if let Some(test) = env.storage().instance().get(&DataKey::MedicalTest(test_id)) {
+                tests.push_back(test);
+            }
+        }
+
+        tests
+   }
+
+   pub fn get_all_doctor_medical_records(env: Env, doctor_id: u64) -> Vec<MedicalTest> {
+    if !env.storage().instance().has(&DataKey::Doctor(doctor_id)) {
+        panic!("Doctor not found");
     }
+
+    let test_ids: Vec<u64> = env.storage().instance().get(&DataKey::DoctorTests(doctor_id)).unwrap();
+    let mut tests = Vec::new(&env);
+
+    for test_id in test_ids.iter() {
+        if let Some(test)  = env.storage().instance().get(&DataKey::MedicalTest(test_id)) {
+            tests.push_back(test);
+        }
+    }
+
+    tests
     }  
 
     pub fn get_all_medical_test_records(env: Env) -> Vec<MedicalTest> {
